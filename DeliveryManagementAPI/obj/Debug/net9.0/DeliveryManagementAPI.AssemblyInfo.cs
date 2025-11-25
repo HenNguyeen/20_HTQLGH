4c8ac4b174d54f11e39cd192189b4e7bba4d4243
Hên <23DH111009@st.huflit.edu.vn>
2025-11-25 13:10:45 +0700

    Cập  nhật

--- DeliveryManagementAPI/obj/Debug/net9.0/DeliveryManagementAPI.AssemblyInfo.cs
+++ DeliveryManagementAPI/obj/Debug/net9.0/DeliveryManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliveryManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a4e52d323d617f048080f51960284564d79535e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4544e767732a23d19b783e0be4cbed45ae78708e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliveryManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliveryManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
